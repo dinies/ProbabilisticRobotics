e05d550d8021bb3e4ab53a808dfed2d402cb3d30
dinies <edoardo.ghini@live.it>
2018-05-18 18:09:41 +0200

    added some slides to the presentation ProSlam

--- ProSlamPresentation/auto/ProSlamSlides.el
+++ ProSlamPresentation/auto/ProSlamSlides.el
@@ -1,6 +1,8 @@
 (TeX-add-style-hook
  "ProSlamSlides"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "16pt")))
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
